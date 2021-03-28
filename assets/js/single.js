de9db7153c60ed8cc6c98c6bf1a8cfdd4b7cd3b8
JSantos <jeremy.santos1010@gmail.com>
2021-03-27 21:55:55 -0400

    allowed index.html js file to be merged with single-repo/single.js file

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,19 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    }
+    else {
+        document.location.replace("./index.html");
+    }
+    
+}
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -20,7 +34,7 @@ var getRepoIssues = function(repo) {
             })
         }
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
@@ -74,4 +88,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
